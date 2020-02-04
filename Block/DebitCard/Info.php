1d7996d4cc901a1b63b26c7ee2b59597625e9e87
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-02-04 16:11:26 -0200

    - Change Copyright

--- Block/DebitCard/Info.php
+++ Block/DebitCard/Info.php
@@ -8,7 +8,7 @@
  *
  * @category  FCamara
  * @package   FCamara_Getnet
- * @copyright Copyright (c) 2019 FCamara Formação e Consultoria
+ * @copyright Copyright (c) 2020 Getnet
  * @Agency    FCamara Formação e Consultoria, Inc. (http://www.fcamara.com.br)
  * @author    Jonatan Santos <jonatan.santos@fcamara.com.br>
  */

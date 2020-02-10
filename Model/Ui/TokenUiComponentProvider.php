eaf4a700a5b415fbbedf6e52c5916e9b17ede32f
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-02-10 19:59:25 -0200

    - Token UI component provider

---
+++ Model/Ui/TokenUiComponentProvider.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to https://www.fcamara.com.br/ for more information.
+ *
+ * @category  FCamara
+ * @package   FCamara_Getnet
+ * @copyright Copyright (c) 2020 Getnet
+ * @Agency    FCamara Formação e Consultoria, Inc. (http://www.fcamara.com.br)
+ * @author    Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
+ */
+
+namespace FCamara\Getnet\Model\Ui;
+
+use Magento\Vault\Model\Ui\TokenUiComponentProviderInterface;
+use Magento\Vault\Api\Data\PaymentTokenInterface;
+use FCamara\Getnet\Model\Ui\CreditCard\ConfigProvider;
+
+class TokenUiComponentProvider implements TokenUiComponentProviderInterface
+{
+    /**
+     * Get UI component for token
+     *
+     * @param PaymentTokenInterface $paymentToken
+     * @return \Magento\Vault\Model\Ui\TokenUiComponentInterface
+     */
+    public function getComponentForToken(PaymentTokenInterface $paymentToken)
+    {
+        $jsonDetails = json_decode($paymentToken->getTokenDetails() ?: '{}', true);
+        $component = $this->componentFactory->create(
+            [
+                'config' => [
+                    'code' => ConfigProvider::CC_VAULT_CODE,
+                    TokenUiComponentProviderInterface::COMPONENT_DETAILS => $jsonDetails,
+                    TokenUiComponentProviderInterface::COMPONENT_PUBLIC_HASH => $paymentToken->getPublicHash()
+                ],
+                'name' => 'Magento_Braintree/js/view/payment/method-renderer/vault'
+            ]
+        );
+
+        return $component;
+    }
+}

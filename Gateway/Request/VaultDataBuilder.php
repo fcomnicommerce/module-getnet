58371c25ced549709efe7b7292c674c633367b25
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-02-10 19:19:11 -0200

    - Add VaultDataBuilder

---
+++ Gateway/Request/VaultDataBuilder.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to https://www.fcamara.com.br/ for more information.
+ *
+ * @category  FCamara
+ * @package   FCamara_Getnet
+ * @copyright Copyright (c) 2020 Getnet
+ * @Agency    FCamara Formação e Consultoria, Inc. (http://www.fcamara.com.br)
+ * @author    Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
+ */
+
+namespace FCamara\Getnet\Gateway\Request;
+
+use Magento\Payment\Gateway\Request\BuilderInterface;
+
+class VaultDataBuilder implements BuilderInterface
+{
+    /**
+     * Additional options in request to gateway
+     */
+    const OPTIONS = 'options';
+
+    /**
+     * The option that determines whether the payment method associated with
+     * the successful transaction should be stored in the Vault.
+     */
+    const STORE_IN_VAULT_ON_SUCCESS = 'storeInVaultOnSuccess';
+
+    /**
+     * @inheritdoc
+     */
+    public function build(array $buildSubject)
+    {
+        return [
+            self::OPTIONS => [
+                self::STORE_IN_VAULT_ON_SUCCESS => true
+            ]
+        ];
+    }
+}

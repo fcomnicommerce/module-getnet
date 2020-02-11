830a1fe00832fe2adbbd6cd3b63458b95e6d7acf
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-02-11 15:30:23 -0200

    - Display the stored information

---
+++ Block/Customer/CardRenderer.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to https://www.fcamara.com.br/ for more information.
+ *
+ * @category  FCamara
+ * @package   FCamara_Getnet
+ * @copyright Copyright (c) 2020 Getnet
+ * @Agency    FCamara Formação e Consultoria, Inc. (http://www.fcamara.com.br)
+ * @author    Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
+ */
+
+namespace FCamara\Getnet\Block\Customer;
+
+use FCamara\Getnet\Model\Ui\ConfigProvider;
+use Magento\Framework\View\Element\Template;
+use Magento\Vault\Api\Data\PaymentTokenInterface;
+use Magento\Vault\Block\AbstractCardRenderer;
+
+class CardRenderer extends AbstractCardRenderer
+{
+    /**
+     * Can render specified token
+     *
+     * @param PaymentTokenInterface $token
+     * @return boolean
+     */
+    public function canRender(PaymentTokenInterface $token)
+    {
+        return $token->getPaymentMethodCode() === ConfigProvider::CC_VAULT_CODE;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumberLast4Digits()
+    {
+        return $this->getTokenDetails()['maskedCC'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getExpDate()
+    {
+        return $this->getTokenDetails()['expirationDate'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getIconUrl()
+    {
+        return $this->getIconForType($this->getTokenDetails()['type'])['url'];
+    }
+
+    /**
+     * @return int
+     */
+    public function getIconHeight()
+    {
+        return $this->getIconForType($this->getTokenDetails()['type'])['height'];
+    }
+
+    /**
+     * @return int
+     */
+    public function getIconWidth()
+    {
+        return $this->getIconForType($this->getTokenDetails()['type'])['width'];
+    }
+}
+

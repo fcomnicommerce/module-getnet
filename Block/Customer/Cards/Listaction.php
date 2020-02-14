351e6d7d8bacf204fe77f9bf7a6e035dddd31884
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-02-14 13:52:07 -0200

    - Create controller, block, template for Cards List

---
+++ Block/Customer/Cards/Listaction.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to https://www.fcamara.com.br/ for more information.
+ *
+ * @category  FCamara
+ * @package   FCamara_Getnet
+ * @copyright Copyright (c) 2020 Getnet
+ * @Agency    FCamara Formação e Consultoria, Inc. (http://www.fcamara.com.br)
+ * @author    Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
+ */
+
+namespace FCamara\Getnet\Block\Customer\Cards;
+
+use \Magento\Framework\View\Element\Template;
+
+class Listaction extends Template
+{
+
+}

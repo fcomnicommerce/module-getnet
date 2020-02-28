1833320840e5000d0621e1946fb7a74e8fbacfb2
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-02-28 15:46:16 -0300

    - Create page my subscriptions

---
+++ Block/Customer/Subscriptions/Listaction.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to https://www.fcamara.com.br/ for more information.
+ *
+ * @category  FCamara
+ * @package   FCamara_Getnet
+ * @copyright Copyright (c) 2020 Getnet
+ * @Agency    FCamara Formação e Consultoria, Inc. (http://www.fcamara.com.br)
+ * @author    Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
+ */
+
+namespace FCamara\Getnet\Block\Customer\Subscriptions;
+
+use \Magento\Framework\View\Element\Template;
+use FCamara\Getnet\Model\Client;
+use \Magento\Customer\Model\Session;
+
+class Listaction extends Template
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    private $customerSession;
+
+    /**
+     * Listaction constructor.
+     * @param Template\Context $context
+     * @param Client $client
+     * @param Session $customerSession
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        Client $client,
+        Session $customerSession,
+        array $data = []
+    ) {
+        $this->client = $client;
+        $this->customerSession = $customerSession;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return bool|mixed
+     */
+    public function getSubscriptionsList()
+    {
+        $subscriptions = [];
+        return $subscriptions;
+    }
+}
---
+++ Controller/Subscriptions/Listaction.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to https://www.fcamara.com.br/ for more information.
+ *
+ * @category  FCamara
+ * @package   FCamara_Getnet
+ * @copyright Copyright (c) 2020 Getnet
+ * @Agency    FCamara Formação e Consultoria, Inc. (http://www.fcamara.com.br)
+ * @author    Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
+ */
+
+namespace FCamara\Getnet\Controller\Subscriptions;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Listaction extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $pageFactory;
+
+    /**
+     * Listaction constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
+        $this->pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}

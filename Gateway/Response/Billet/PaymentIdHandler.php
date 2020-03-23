cbe62dc7893618ef76967d3dbf2a7ac0976d237c
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-03-23 15:06:53 -0300

    - Config DI Response

--- Gateway/Response/Billet/PaymentIdHandler.php
+++ Gateway/Response/Billet/PaymentIdHandler.php
@@ -10,13 +10,34 @@
  * @package   FCamara_Getnet
  * @copyright Copyright (c) 2020 Getnet
  * @Agency    FCamara Formação e Consultoria, Inc. (http://www.fcamara.com.br)
- * @author    Jonatan Santos <jonatan.santos@fcamara.com.br>
+ * @author    Danilo Cavalcanti <danilo.moura@fcamara.com.br>
  */
 
 namespace FCamara\Getnet\Gateway\Response\Billet;
 
+use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
+use Magento\Payment\Gateway\Response\HandlerInterface;
+use Magento\Sales\Model\Order\Payment;
 
-class PaymentIdHandler
+class PaymentIdHandler implements HandlerInterface
 {
-
+    /**
+     * @param array $handlingSubject
+     * @param array $response
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function handle(array $handlingSubject, array $response)
+    {
+        /** @var PaymentDataObjectInterface $paymentDataObject */
+        $paymentDataObject = $handlingSubject['payment'];
+        /** @var Payment $payment */
+        $payment = $paymentDataObject->getPayment();
+        if (
+            isset($response["object"]) &&
+            isset($response["object"]["payment_id"])
+        ) {
+            $payment->setAdditionalInformation("payment_id", $response["object"]["payment_id"]);
+            $payment->setAdditionalInformation("billet_data", $response["object"]["boleto"]);
+        }
+    }
 }

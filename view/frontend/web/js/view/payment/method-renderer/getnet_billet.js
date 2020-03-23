32f7b199ffcf269b32c0c8f2b93eab983576fe79
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-03-23 14:12:53 -0300

    - Config JS

--- view/frontend/web/js/view/payment/method-renderer/getnet_billet.js
+++ view/frontend/web/js/view/payment/method-renderer/getnet_billet.js
@@ -6,9 +6,10 @@
 /*global define*/
 define(
     [
+        'jquery',
         'Magento_Checkout/js/view/payment/default'
     ],
-    function (Component) {
+    function ($, Component) {
         'use strict';
 
         return Component.extend({
@@ -18,7 +19,6 @@ define(
             },
 
             initObservable: function () {
-
                 this._super()
                     .observe([
                         'transactionResult'
@@ -26,8 +26,50 @@ define(
                 return this;
             },
 
+            /**
+             * Check if payment is active
+             *
+             * @returns {Boolean}
+             */
+            isActive: function () {
+                var active = this.getCode() === this.isChecked();
+
+                this.active(active);
+
+                return active;
+            },
+
+            /**
+             * Get authorization
+             * @returns {Object}
+             */
+            getAuthorizationBasic: function () {
+                return window.checkoutConfig.payment.getnet_credit_card.authorizationBasic;
+            },
+
+            /**
+             * Get authorization
+             * @returns {Object}
+             */
+            getEndpoint: function () {
+                return window.checkoutConfig.payment.getnet_credit_card.endpoint;
+            },
+
             getCode: function() {
                 return 'getnet_billet';
+            },
+
+            /**
+             * Show error messages
+             *
+             * @param {String[]} errorMessages
+             */
+            _showErrors: function (errorMessages) {
+                $.each(errorMessages, function (index, message) {
+                    globalMessageList.addErrorMessage({
+                        message: message
+                    });
+                });
             },
 
             getData: function() {
@@ -38,7 +80,6 @@ define(
                     }
                 };
             },
-
         });
     }
 );

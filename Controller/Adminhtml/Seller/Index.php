413b49505121b7a73e874cb97a32a0c798c6cd62
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-04-08 17:42:29 -0300

    - Update label

--- Controller/Adminhtml/Seller/Index.php
+++ Controller/Adminhtml/Seller/Index.php
@@ -42,7 +42,7 @@ class Index extends \Magento\Backend\App\Action
     public function execute()
     {
         $resultPage = $this->resultPageFactory->create();
-        $resultPage->getConfig()->getTitle()->prepend((__('Posts')));
+        $resultPage->getConfig()->getTitle()->prepend((__('Seller')));
 
         return $resultPage;
     }

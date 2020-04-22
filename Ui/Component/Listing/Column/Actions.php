ce6fd18935c47012836917ab4e6a11eba2482ff6
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-04-22 14:21:38 -0300

    - Add columns actions

--- Ui/Component/Listing/Column/Actions.php
+++ Ui/Component/Listing/Column/Actions.php
@@ -59,7 +59,7 @@ class Actions extends Column
     public function prepareDataSource(array $dataSource)
     {
         if (isset($dataSource['data']['items'])) {
-            foreach ($dataSource['data']['items'] as $item) {
+            foreach ($dataSource['data']['items'] as & $item) {
                 $item[$this->getData('name')] = [
                     'edit' => [
                         'href' => $this->urlBuilder->getUrl(

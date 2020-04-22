cd69ed44716f7581ca8c88acf6509a88705f0665
Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
2020-04-22 14:20:49 -0300

    - Add columns actions

---
+++ Ui/Component/Listing/Column/Actions.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to https://www.fcamara.com.br/ for more information.
+ *
+ * @category  FCamara
+ * @package   FCamara_
+ * @copyright Copyright (c) 2020 FCamara Formação e Consultoria
+ * @Agency    FCamara Formação e Consultoria, Inc. (http://www.fcamara.com.br)
+ * @author    Danilo Cavalcanti de Moura <danilo.moura@fcamara.com.br>
+ */
+
+namespace FCamara\Getnet\Ui\Component\Listing\Column;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Backend\Block\Widget\Context as ContextBackend;
+
+/**
+ * Class Actions
+ */
+class Actions extends Column
+{
+    /**
+     * @var \Magento\Framework\UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * Actions constructor.
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param ContextBackend $contextBackend
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        ContextBackend $contextBackend,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $contextBackend->getUrlBuilder();
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as $item) {
+                $item[$this->getData('name')] = [
+                    'edit' => [
+                        'href' => $this->urlBuilder->getUrl(
+                            'fcamara_getnet/seller/edit',
+                            ['entity_id' => $item['entity_id']]
+                        ),
+                        'label' => __('Edit')
+                    ],
+                    'remove' => [
+                        'href' => $this->urlBuilder->getUrl(
+                            'fcamara_getnet/seller/deleteaction',
+                            ['entity_id' => $item['entity_id']]
+                        ),
+                        'label' => __('Remove')
+                    ]
+                ];
+            }
+        }
+
+        return $dataSource;
+    }
+}
+

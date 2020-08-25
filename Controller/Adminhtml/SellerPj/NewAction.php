21fd06ab5b40950a42f616f1802f4a0d3c4e7361
Danilo Moura <danilo-cm@hotmail.com>
2020-08-25 19:24:07 -0300

    - Validate errors

--- Controller/Adminhtml/SellerPj/NewAction.php
+++ Controller/Adminhtml/SellerPj/NewAction.php
@@ -81,6 +81,12 @@ class NewAction extends \Magento\Backend\App\Action
                         }
                     }
 
+                    if (isset($integratedSeller['errors'])) {
+                        foreach ($integratedSeller['errors'] as $error) {
+                            $this->messageManager->addErrorMessage($error);
+                        }
+                    }
+
                     throw new \Exception(__('Error Create Seller, Please try again!'));
                 }
 
